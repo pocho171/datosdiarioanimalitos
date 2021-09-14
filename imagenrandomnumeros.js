0a5877650a903fd7634f4c8f676a29574a42f432
pocho171 <32076706+pocho171@users.noreply.github.com>
2021-09-14 19:58:56 -0400

    Add files via upload

---
+++ imagenrandomnumeros.js
@@ -0,0 +1,24 @@
+function makeArray(n) {
+this.length = n;
+for (var i=1; i<=n; i++) {
+this[i] = "";
+}
+return this;
+}
+var i;
+function imagealeatoire() {
+i = Math.floor(10 * Math.random() + 1);
+return image[i];
+}
+image = new makeArray(10);
+image[1]="https://lh3.googleusercontent.com/R_6jj8sriQJrJyaXRW_MtgUkiT6grefvqw4rxjCjGPXIHe8bXd6iWXOssdSGTKN-5S4UIKoUai_-jOxJ_GWSwMTD3T7fa0t5Ul3OTutqMlWBMtZnNcTeLaARFzY4deyEcGbXKW2eug=w1920-h1080";
+image[2]="https://lh3.googleusercontent.com/j93kjIp97N-6zQLnevYlaX14WoRI2ujJ9S2TukctMrha9PhxUlq_JAY58jDXVZnBBEqLAgOt3NKH43wA_HLmzvAp7h6WVqcEroh4GkkaCVtmsGtsllcAeTWOZt5SPWMdnARfErGa2g=w1920-h1080";
+image[3]="https://lh3.googleusercontent.com/zTqVD5jSDUHACcr3HtzJ3-COsQlrtZClgUT40Sd3XHfXmQez-PSkWv7sotSxwWeSpMiEWPTxDiYUx5xHubncjTLVJ8iqu-7tv8LJ6WDy2IIVMmC9ePVinI6amYM4x1K8pQJOzvIFJQ=w1920-h1080";
+image[4]="https://lh3.googleusercontent.com/ccoPa14ozQlIJLT1CqR6Fiq0Ucp3yThnGaxNUIi10w2TnJ29IdiNJBwsVZ5Uv9kPx7Lj2yLiKM6iijz3AGZ9IWWo0v6tUb3mNZ8j75l-wjakPhBLvOo5pxXrRsD4zxnoVHxnfqxGFw=w1920-h1080";
+image[5]="https://lh3.googleusercontent.com/5-g2CVo34cwIjAC7Dmuix2Z0OcvUoQMFzUgduSoQOzwLdd88xcjIWmLUJsFc0Xg3zwqQRrxfGhkijsDAluSsD_UcyFyPXCOQ0R4lesuzh_Am0ZR4OZdD2b78YaCI3MPnS8sbiElOuw=w1920-h1080";
+image[6]="https://lh3.googleusercontent.com/Wgdg3WVwk5LpbgIyG2vwcR_SEsXQAOsyy22YuaQY13FQTD65CNS0rSA3dLaob1_1WMw4Ps1FHV-UMHwnbTgOBbMue9mT3iVJ7QbQOwEhMTH6U21SSaolfBPvIRc-4iJEnLkdBBEUCg=w1920-h1080";
+image[7]="https://lh3.googleusercontent.com/FEEDUyoIW_ImFRQ9hwb_xXQPGMemKMeZYe5dGD78nOe9a7cxIm6T7gfvtbdhiybZ7YxS8_emryw1Pj_hdl0bXnnj9Sdxufw-_tKupp08Ms5I_Nh8lfLKcWTa_V3UP_WyPyiBszYC-Q=w1920-h1080";
+image[8]="https://lh3.googleusercontent.com/s-ehaZhy6DQj1NrfmOkXf0PJef3zLdx9URmvfjkIdSIn8E1GfDEpb-0Vg-om7XS8QoSCYSJe9w9MVmK8bdeevf-bMnIfdRoZ8aPRk30q4ZLRgeOCVN2YDzdUZDPg-Ypv_OlkF2r5rw=w1920-h1080";
+image[9]="https://lh3.googleusercontent.com/EYBUcT4umFNy0to-qlRk6SPFNbX7Z52lrzjeZjwAxn7YmVQKRVFN0cOe4juimh5oae6XqXzgSAOIvijASOwAlPfQDL3PccGe5PmfHiUEHW_ifGm-88oQqndDaDCIDiv2xk3eaT35lw=w1920-h1080";
+image[10]="https://lh3.googleusercontent.com/5JlmS0co-DT4e_NySLIEsvB5dkqYYUga6bNK9yIotdg1IxHjzWKsNutc_3wFXjHuqOvjC1fb_AL_BNbDstqr04AB-nuhwhIQjjtFMKuTLKbKQHbkW41c-A-dAYAYVRre9aBmkmNaZg=w1920-h1080";
+//
